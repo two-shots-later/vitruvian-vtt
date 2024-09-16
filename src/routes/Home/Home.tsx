8a4f4ea6edc22d5eb775f372e6eb92689aac4339
Brooks Palin <brookspalin710@gmail.com>
2024-09-16 17:36:31 -0600

    added various icons from fa, this is just for starting out

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -9,7 +9,6 @@ const Home = () => {
       <h1>Home</h1>
       <p>Home page content</p>
       <Link to='/component-lib'>Go to Component Lib</Link>
-      <Icon variant="d12" size={100}/>
       <div className="grid grid-flow-row grid-cols-4 gap-2"> 
         <Badge variant="d20" size={100}/>
         <Badge variant="d12" size={100}/>
@@ -17,6 +16,16 @@ const Home = () => {
         <Badge variant="d8" size={100}/>
         <Badge variant="d6" size={100}/>
         <Badge variant="d4" size={100}/>
+        <Badge variant="warning" size={100}/>
+        <Badge variant="check" size={100}/>
+        <Badge variant="info" size={100}/>
+        <Badge variant="user" size={100}/>
+        <Badge variant="caret-up" size={100}/>
+        <Badge variant="filter" size={100}/>
+        <Badge variant="arrow-right" size={100}/>
+        <Badge variant="search" size={100}/>
+        <Badge variant="pen" size={100}/>
+        <Badge variant="gear" size={100}/>
       </div>
     </div>
   )
--- src/types/Icon.ts
+++ src/types/Icon.ts
@@ -1,1 +1,1 @@
-export type IconId = "d20" | "d12" | "d10" | "d6" | "d8" | "d4";
+export type IconId = "d20" | "d12" | "d10" | "d6" | "d8" | "d4" | "warning" | "check" | "info" | "user" | "caret-up" | "filter" | "arrow-right" | "search" | "pen" | "gear";
